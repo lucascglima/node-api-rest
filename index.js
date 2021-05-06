62cb043cfaa552415abcc972e44403685e1c62cb
Lucas Lima <lucascglima@gmail.com>
2021-05-06 16:15:31 -0300

    realizando post e get

--- index.js
+++ index.js
@@ -1,5 +1,16 @@
 const customExpress = require('./config/customExpress')
+const conexao = require('./infraestrutura/conexao')
+const Tabelas = require('./infraestrutura/tabelas')
 
-const app = customExpress()
+conexao.connect((erro) => {
+  if (erro) {
+    console.log(erro)
+  } else {
 
-app.listen(3000, () => console.log('Servidor rodando na porta 3000'))
+    console.log('conectado com sucesso')
+    Tabelas.init(conexao)
+    const app = customExpress()
+    app.listen(3000, () => console.log('Servidor rodando na porta 3000'))
+
+  }
+})
